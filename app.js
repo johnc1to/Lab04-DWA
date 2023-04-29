6c89527ba9f57bfaa674aec6c14dcdc7282e2e7f
John <pierojbh@gmail.com>
2023-04-28 22:19:01 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/font-awesome', express.static(__dirname + '/font-awesome'));
+
+io.on('connection', (socket) => {
+    // Procedimiento 4:
+   socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
+})
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${__dirname}/`)
+})
+
+server.listen(3000,() => {
+    console.log('Servidor corriendo en http://localhost:3000')
+})
